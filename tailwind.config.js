8699a67f822d722b686660779e1da3b8cec701aa
Rhodney Dame N. Ponsica <93698010+Rdpons@users.noreply.github.com>
2024-12-13 16:29:50 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Arial', 'sans-serif'],
+      },
+      animations: {
+        
+      }
+    },
+  },
+  plugins: [],
+}
+
